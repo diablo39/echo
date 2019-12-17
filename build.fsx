b20896013b56c12e68087de5d6a45ed5f43c1071
Roman Glogowski <18666546+diablo39@users.noreply.github.com>
2019-12-17 21:13:52 +0100

    add tests

--- build.fsx
+++ build.fsx
@@ -1,14 +1,18 @@
-#r "paket:
-nuget Fake.DotNet.Cli
-nuget Fake.IO.FileSystem
-nuget Fake.Core.Target //"
+#r "paket:nuget Fake.DotNet.Cli"
+#r "paket:nuget Fake.IO.FileSystem"
+#r "paket:nuget Fake.DotNet.Testing.XUnit2"
+#r "paket:nuget Fake.Core.Target"
+
 #load ".fake/build.fsx/intellisense.fsx"
+
+// Clean ->  Restore all -> Build app -> build & run unit tests -> build & run integration tests -> publish -> docker build -> docker publish
 open Fake.Core
 open Fake.DotNet
 open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
+open Fake.DotNet.Testing.XUnit2
 
 Target.initEnvironment ()
 
@@ -17,16 +21,41 @@ Target.create "Clean" (fun _ ->
     ++ "src/**/obj"
     |> Shell.cleanDirs 
 )
+Target.create "Restore" (fun _ ->
+    !! "*.sln"
+    |> Seq.iter (DotNet.restore (fun args -> 
+    { args with 
+        Sources = [
+            "https://api.nuget.org/v3/index.json"
+        ]
+    }) )
+)
 
 Target.create "Build" (fun _ ->
     !! "src/**/*.*proj"
+    ++ "tests/**/*.*proj"
     |> Seq.iter (DotNet.build id)
 )
 
+Target.create "Run tests" (fun _ ->
+
+    !! ("src/**/*.*proj" @@ "xUnit.Test.*.dll")
+    |> xUnit2 (fun p -> { p with HtmlOutputPath = Some (testDir @@ "xunit.html") })
+
+    
+)
+
+
 Target.create "All" ignore
 
+
 "Clean"
-  ==> "Build"
-  ==> "All"
+    ==> "Restore"
+    ==> "Build" 
+    ==> "Run integration test"
+//     ==> "UnitTests" <=> "IntegrationsTests"
+//         ==> "Docker build"
+//         ==> "Docker publish"
+         ==> "All"
 
 Target.runOrDefault "All"
---
+++ tests/Roman.Echo.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Roman.Echo.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
